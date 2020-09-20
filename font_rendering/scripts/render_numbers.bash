c4d7e457dd8533f32e9ac87db538085287f93211
Vasu Agrawal <vasu_agrawal@yahoo.com>
2020-09-20 03:50:08 -0400

    render atlas

---
+++ font_rendering/scripts/render_numbers.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Enter output path"
+    exit
+fi
+
+for i in {0..9}; do
+    output="$1/$i"
+    ../build/RenderAtlas --font-dir /data/datasets/fonts/by_extension \
+        --output-dir $output \
+        --count 0 \
+        --dpi 300 \
+        --point 12 \
+        --border 0 \
+        --padding 0 \
+        --atlas "$i"
+
+    jdupes --delete --noprompt --recurse $output > /dev/null
+done
+
--- font_rendering/src/render_atlas.cpp
+++ font_rendering/src/render_atlas.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <thread>
 
+#include "font_rendering/recursive_font_mapper.h"
 #include "font_rendering/renderer.h"
 namespace fs = std::filesystem;
 
@@ -12,15 +13,19 @@ namespace fs = std::filesystem;
 
 #include <opencv2/highgui.hpp>
 
-DEFINE_int32(thread_count, 24, "Number of render threads to use");
-DEFINE_int32(count, 0, "Number of files to stop after");
 DEFINE_string(font_dir, "", "Path to font directory");
 DEFINE_string(output_dir, "", "Path to save output images");
+DEFINE_string(atlas, "", "Override atlas");
+
+DEFINE_int32(dpi, 300, "DPI to use for font rendering");
+DEFINE_int32(point, 12, "Point size for font rendering");
+DEFINE_int32(border, RendererSpacing::DEFAULT, "Border for font rendering");
+DEFINE_int32(padding, RendererSpacing::DEFAULT, "Padding for font rendering");
 DEFINE_uint32(write_style, 0, "Image write style (0-3)");
 
-static const std::string ERROR_DIR_NAME = "errors";
-inline static const std::vector<std::string> font_extensions{
-    ".otf", ".ttf", ".svg", ".eot", ".woff", ".woff2"};
+DEFINE_int32(thread_count, 24, "Number of render threads to use");
+DEFINE_int32(count, 0, "Number of files to stop after");
+
 static constexpr size_t QUEUE_SIZE = 10;
 
 int main(int argc, char* argv[]) {
@@ -41,118 +46,38 @@ int main(int argc, char* argv[]) {
   FLAGS_log_dir = FLAGS_output_dir;
   google::InitGoogleLogging(argv[0]);
 
-  // Create the render queues
-  std::vector<std::unique_ptr<folly::ProducerConsumerQueue<std::string>>>
-      render_queues;
-  render_queues.reserve(FLAGS_thread_count);
-  for (int i = 0; i < FLAGS_thread_count; ++i) {
-    render_queues.emplace_back(
-        std::make_unique<folly::ProducerConsumerQueue<std::string>>(
-            QUEUE_SIZE));
-  }
-
   fs::path output_dir(FLAGS_output_dir);
   fs::create_directories(output_dir);
-  fs::path error_dir = output_dir / ERROR_DIR_NAME;
-  for (const auto& s : RendererErrorNames) {
-    fs::create_directories(output_dir / ERROR_DIR_NAME / s);
+
+  auto atlas = makeFullAtlas();
+  if (FLAGS_atlas != "") {
+    std::vector<std::string> user_atlas;
+    std::istringstream stream(FLAGS_atlas);
+    std::string line;
+    while (std::getline(stream, line, '\n')) {
+      user_atlas.push_back(line);
+    }
+
+    atlas = user_atlas;
   }
-  std::atomic_bool new_work{true};
 
-  // Create the renderer threads
-  std::vector<std::thread> render_threads;
-  render_threads.reserve(FLAGS_thread_count);
+  RendererSpacing spacing(FLAGS_dpi, FLAGS_point, FLAGS_border, FLAGS_padding);
+
+  std::vector<Renderer> renderers;
+  renderers.reserve(FLAGS_thread_count);
   for (int i = 0; i < FLAGS_thread_count; ++i) {
-    render_threads.emplace_back([&new_work, &render_queues, i, output_dir]() {
-      Renderer r;
-      std::string canonical;
-      const std::vector<int> compression_params{cv::IMWRITE_PNG_COMPRESSION, 9};
-      while (true) {
-        if (!render_queues[i]->read(canonical)) {          // failed to get work
-          if (new_work.load(std::memory_order_acquire)) {  // more to come
-            using namespace std::chrono_literals;
-            std::this_thread::sleep_for(1ms);
-            continue;
-          } else {  // Nothing else is coming, we're done
-            break;
-          }
-        }
+    renderers.emplace_back(Renderer(atlas, spacing));
+  }
 
-        // Not checking error condition here since it's checked in render call
+  RecursiveFontMapper mapper(FLAGS_thread_count);
+  mapper.runAndWait(
+      [&](int32_t thread_index, const std::string& canonical) {
+        auto& r = renderers[thread_index];
         r.loadFontFace(canonical);
         auto [mat, err] = r.renderAtlas();
-
-        const auto output_basename = fs::path(canonical).stem().string();
-        fs::path output_dirname = output_dir;
-        if (auto e = static_cast<int>(err); e) {
-          LOG(WARNING) << fmt::format("Issue while rendering font {}: {}",
-                                      canonical, RendererErrorStrings[e]);
-          output_dirname =
-              output_dirname / ERROR_DIR_NAME / RendererErrorNames[e];
-        }
-
-        r.saveImage(output_dirname, output_basename, mat,
+        const auto output_basename = fs::path(canonical).filename().string();
+        r.saveImage(output_dir, output_basename, mat,
                     static_cast<ImageWriteStyle>(FLAGS_write_style));
-      }
-    });
-  }
-
-  int count = 0;
-  for (auto& p : fs::recursive_directory_iterator(FLAGS_font_dir)) {
-    if (FLAGS_count > 0 && count >= FLAGS_count) {
-      break;
-    }
-
-    // Only consider regular files that have the right extension
-    if (!fs::is_regular_file(p) ||
-        std::find(font_extensions.begin(), font_extensions.end(),
-                  p.path().extension()) == font_extensions.end()) {
-      continue;
-    }
-
-    ++count;
-
-    const auto canonical = p.path().string();
-
-    // Use the count to pick a queue to add the path to. If that one's full,
-    // iterate through the rest of them. If they're all full, wait a second,
-    // and repeat, ad infineum.
-    while (true) {
-      // Try all the queues once, starting with the one we're supposed to
-      for (int i = 0; i < render_queues.size(); ++i) {
-        const auto queue_index = (count + i) % render_queues.size();
-        if (render_queues[queue_index]->write(canonical)) {
-          goto loaded;
-        }
-      }
-
-      // And if we're not able to put a thing into a queue after cycling
-      // through them all once, take a breather.
-      using namespace std::chrono_literals;
-      std::this_thread::sleep_for(1ms);
-    }
-
-  loaded:;
-  }
-
-  new_work.store(false, std::memory_order_release);
-  for (int i = 0; i < render_threads.size(); ++i) {
-    if (render_threads[i].joinable()) {
-      render_threads[i].join();
-    }
-  }
-
-  // Remove the empty error dirs
-  for (auto& p : fs::directory_iterator(output_dir / ERROR_DIR_NAME)) {
-    if (fs::is_directory(p.path()) && fs::is_empty(p.path())) {
-      LOG(INFO) << "Removing empty error folder " << p;
-      fs::remove(p.path());
-    }
-  }
-
-  // Remove error dir itself if empty
-  if (fs::is_directory(error_dir) && fs::is_empty(error_dir)) {
-    LOG(INFO) << "Removing empty error dir " << error_dir;
-    fs::remove(error_dir);
-  }
+      },
+      FLAGS_font_dir, FLAGS_count);
 }
